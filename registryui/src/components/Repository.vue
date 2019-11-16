0ebe4063aacae9cc7f81b28d391bf34980db7e52
readerx <gnolld@foxmail.com>
2019-11-16 18:17:15 +0800

    add vue init

---
+++ registryui/src/components/Repository.vue
@@ -0,0 +1,37 @@
+<template>
+  <el-table :data="tableData" stripe style="width: 100%">
+    <el-table-column prop="name" label="仓库"></el-table-column>
+    <el-table-column fixed="right" label="操作" width="100">
+      <template slot-scope="scope">
+        <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
+        <el-button type="text" size="small">编辑</el-button>
+      </template>
+    </el-table-column>
+  </el-table>
+</template>
+
+<script>
+export default {
+  name: "Repository",
+
+  methods: {
+    handleClick(row) {
+      // eslint-disable-next-line no-console
+      console.log(row.name);
+    }
+  },
+
+  data() {
+    return {
+      tableData: [
+        {
+          name: "coredns"
+        },
+        {
+          name: "test/abc"
+        }
+      ]
+    };
+  }
+};
+</script>
---
+++ registryui/src/layouts/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <div id="app">
+    <el-container>
+      <el-header>Repository</el-header>
+      <Repository></Repository>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import Repository from "./components/Repository.vue";
+
+export default {
+  name: "app",
+  components: {
+    Repository
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
+<style>
+.el-header,
+.el-footer {
+  background-color: #b3c0d1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-aside {
+  background-color: #d3dce6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+}
+
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
+}
+</style>
